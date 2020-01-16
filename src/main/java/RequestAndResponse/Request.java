7b3d27cca273eb43a6d44fc9870cdf163674e377
Razine Bensari <bensaria97@gmail.com>
2020-01-16 13:46:17 -0500

    adding cli options

--- src/main/java/RequestAndResponse/Request.java
+++ src/main/java/RequestAndResponse/Request.java
@@ -1,5 +1,7 @@
 package RequestAndResponse;
 
+import picocli.CommandLine.Option
+
 import java.io.File;
 import java.net.URL;
 import java.util.HashMap;
@@ -16,6 +18,7 @@ public final class Request {
         URL url = builder.url;
         Method httpMethod = builder.httpMethod;
         Map<String, String> headers = builder.headers;
+        Map<String, String> queryParameters = builder.queryParameters;
         String body = builder.body;
         File file = builder.file;
         String redirectUrl = builder.redirectUrl;
@@ -25,8 +28,17 @@ public final class Request {
 
         private URL url;
         private Method httpMethod;
-        private Map<String, String> headers = new HashMap<String, String>();
+
+        @Option(names = {"-h", "--headers"}, description = "Associates headers to HTTP Request with the format 'key:value'.")
+        private Map<String, String> headers;
+
+        @Option(names = {"-q", "--query"}, description = "Appends the query to the associated url.")
+        private Map<String, String> queryParameters;
+
+        @Option(names = {"-d", "--data"}, description = "Associates an inline data to the body HTTP POST request.")
         private String body;
+
+        @Option(names = {"-f", "--file"}, description = "Associates the content of a file to the body HTTP POST.")
         private File file;
         private String redirectUrl;
 
@@ -61,6 +73,11 @@ public final class Request {
 
         public Builder withHeaders(Map<String, String> headers){
             this.headers = headers;
+            return this;
+        }
+
+        public Builder withQueryParameters(Map<String, String> queryParameters){
+            this.queryParameters = queryParameters;
             return this;
         }
 
