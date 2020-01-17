67eadae0df03daf33b72106cfd82bdcd3d475d82
Razine Bensari <bensaria97@gmail.com>
2020-01-17 17:12:18 -0500

    clean up

---
+++ sh/JarToBinary.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#The purpose of this file is to convert the jar file built by maven into a binary
+# so we can use the httpc commands without going through the complicated and long java -cp *** commands
+
+echo "Removing potential broken binary"
+rm -r ./../bin/httpc
+echo "Creating binary of httpc from jar file"
+cat ./httpc.sh ./../target/httpc-1-jar-with-dependencies.jar > httpc && chmod +x httpc
+echo "Binary Created!"
+echo "Moving Binary to /bin folder..."
+mv ./httpc ./../bin/
